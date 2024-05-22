7465e076abdbd87be772ff2dd874b5aacec8a76d
rishiirajanand <rishirajanand5@gmail.com>
2024-05-22 16:38:13 +0530

    project completed

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import ButtonBox from "./Components/ButtonBox";
+import Screen from "./Components/Screen"
+import Wrapper from "./Components/Wrapper"
+import Button from "./Components/Button";
+import CalcProvider from "./Context/CalcContext";
+
+function App() {
+
+  const btnValues = [
+    ['C', '+-', '%', '/'],
+    [7, 8, 9, 'x'],
+    [4, 5, 6, '-'],
+    [1, 2, 3, '+'],
+    [0, '.', '=']
+  ];
+
+  return (
+
+    < CalcProvider >
+    
+      <Wrapper >
+        < Screen />
+        < ButtonBox >
+          {btnValues.flat().map((btn, index)=> (
+            < Button key={index} value ={btn}/>
+          ))}
+        </ButtonBox>
+      </Wrapper>
+
+    </CalcProvider>
+  )
+}
+
+export default App
---
+++ src/Components/Button.jsx
@@ -0,0 +1,123 @@
+import React, { useContext } from 'react'
+import {CalcContext} from '../Context/CalcContext'
+
+const getStyleName = btn =>{
+    const className = {
+        '=' : 'equals',
+        'x' : 'opt',
+        '+' : 'opt',
+        '-' : 'opt',
+        '/' : 'opt',
+    }
+    return className[btn];
+}
+
+
+function Button({value}) {
+
+  const {calc,setCalc} = useContext(CalcContext)
+  
+  // User click comma
+  const commaClick = ()=>{
+    setCalc({
+      ...calc,
+      num : !calc.num.toString().includes('.') ? calc.num + value : calc.num
+    })
+  }
+
+  // User click C
+  const resetClick = ()=>{
+    setCalc({
+      sign: '', num : 0, res: 0
+    })
+  }
+  // User Click Number
+  const handleCilckButton = ()=>{
+    const numberString = value.toString();
+
+    let numberValue;
+
+    if(numberString === '0' && calc.num === 0){
+      numberValue = '0'
+    }else{
+      numberValue = Number(calc.num + numberString)
+    }
+
+    setCalc({
+      ...calc,
+      num : numberValue
+    })
+  }
+
+  // User click operation
+  const signClick = ()=>{
+    setCalc({
+      sign : value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num : 0
+    })
+  }
+  // User click equals
+  const equalsClick = ()=>{
+    if(calc.res && calc.num){
+      const math = (a, b, sign)=>{
+        const result = {
+          '+' : (a,b)=> a + b,
+          '-' : (a,b)=> a - b,
+          'x' : (a,b)=> a * b,
+          '/' : (a,b)=> a / b,
+        }
+        return result[sign](a,b)
+      }
+      setCalc({
+        res : math(calc.res, calc.num, calc.sign),
+        sign : '',
+        num : 0
+      })
+    }
+  }
+
+  // User click percent
+  const percentClick = ()=>{
+    setCalc({
+      num:(calc.num / 100),
+      res : (calc.res / 100),
+      sign : ''
+    })
+  }
+
+  // User click invert
+  const invertClick = ()=>{
+    setCalc({
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign : ''
+    })
+  }
+
+  const handleClick = ()=>{
+    const results = {
+      '.' : commaClick,
+      'C' : resetClick,
+      '/' : signClick,
+      'x' : signClick,
+      '-' : signClick,
+      '+' : signClick,
+      '=' : equalsClick,
+      '%' : percentClick,
+      '+-': invertClick
+    }
+    if(results[value]){
+      return results[value]()
+    }
+    else{
+      return handleCilckButton()
+    }
+  }
+
+  return (
+    <button onClick={handleClick} className={`${getStyleName(value)} button`}>{value}</button>
+  )
+}
+
+export default Button
